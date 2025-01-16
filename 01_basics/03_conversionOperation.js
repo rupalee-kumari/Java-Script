519cb2ac4e0976486e5aba87310cdee48ee9e55f
rupalee-kumari <151054155+rupalee-kumari@users.noreply.github.com>
2025-01-16 19:40:34 +0000

    convesrsion datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,4 +17,20 @@ console.log(booleanIsLoggendIn);//true..  for 0 false...   when empty then false
 let sNumber=33
 let StringNumber=String(sNumber)
 console.log(StringNumber)
-console.log(typeof(StringNumebr));//string
+console.log(typeof(StringNumber));//string
+
+
+
+//console.log(2+2) 4
+//console.log(2-2) 0
+//console.log(2*2) 4
+//console.log(2**3) 8
+ let str1="hello"
+ let str2="rupalee"
+ let str3= str1 + str2 //hellorupalee
+ console.log(str3)
+ console.log("1"+2);//12
+ console.log(1+"2");//12
+ console.log("1"+2+2);//122
+ console.log(1+3+"2");//42
+ 
