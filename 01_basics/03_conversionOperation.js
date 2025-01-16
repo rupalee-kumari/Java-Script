516a79c889b40669fc50e9aee99abb0c4fd30a29
rupalee-kumari <151054155+rupalee-kumari@users.noreply.github.com>
2025-01-16 19:22:36 +0000

    conversion datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score="33abc"
+console.log(typeof score)
+console.log(typeof(score));
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);//NaN
+
+//"33"=convert 33
+//true=1, false=0
+
+let islogin=1
+let booleanIsLoggendIn=Boolean(islogin)
+console.log(booleanIsLoggendIn);//true..  for 0 false...   when empty then false...  when value rupalee then true
+
+
+
+let sNumber=33
+let StringNumber=String(sNumber)
+console.log(StringNumber)
+console.log(typeof(StringNumebr));//string
