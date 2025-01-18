bcbbe21a9298e86e3c03836edea2016d4ac753b6
rupalee-kumari <151054155+rupalee-kumari@users.noreply.github.com>
2025-01-18 11:01:27 +0000

    String

---
+++ 01_basics/05_String.js
@@ -0,0 +1,21 @@
+const name="rupalee"
+const repoCount=50
+//console.log(name+repoCount+" value");
+console.log(`Hello my name is ${name} and my repo is ${repoCount}`);
+const gameName=new String('rupaleer')
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("p"));
+const newString=gameName.substring(0,5);
+console.log(newString);
+const news="   rupalee   "
+console.log(news.trim());
+
+console.log(news);
+const url="https://rupalee.com" 
+console.log(url.replace('ee','i'));
+console.log(url.includes('rupam'));
+console.log(gameName.split('p'));
+
