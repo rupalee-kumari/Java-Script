ec3593db5cb2cf0f469fea2a6aaa46836404b8d1
rupalee-kumari <151054155+rupalee-kumari@users.noreply.github.com>
2025-01-18 11:35:31 +0000

    Date

---
+++ 01_basics/07_Dates.js
@@ -0,0 +1,22 @@
+let myDate=new Date()
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toDateString());
+
+//let mycreatedDate=new Date(2025,0,23,5,3)
+//console.log(mycreatedDate.toLocaleString());
+
+// let mycreatedDate=new Date("01-14-2025")
+// console.log(mycreatedDate.toLocaleString());
+// console.log(mycreatedDate.getTime());
+
+let newDate=new Date()
+console.log(newDate.getDate())
+console.log(newDate.getFullYear());
+console.log(newDate.getDay());
+
+
+
+
+
+
