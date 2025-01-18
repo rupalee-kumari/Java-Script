b4904294edec06a1b74b5e41bee3373f5b42ea05
rupalee-kumari <151054155+rupalee-kumari@users.noreply.github.com>
2025-01-18 11:20:46 +0000

    num and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,26 @@
+const score=400
+const balance=new Number(100);
+// console.log(balance)
+// console.log(score)
+// console.log(balance.toFixed(2));//100.00
+// console.log(balance.toString().length);
+// console.log(balance.toPrecision(3));
+// const othervalue=1000000
+// console.log(othervalue.toLocaleString('en-IN'));//10,00,000
+// console.log(othervalue.toLocaleString());//1,000,000
+
+
+//maths//
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log((Math.floor(4.6)));
+console.log(Math.ceil(4.6))
+console.log(Math.random())
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+
+
+
