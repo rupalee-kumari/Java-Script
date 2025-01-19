4bb48e159f169aad3e4239885755262b17fa41a1
rupalee-kumari <151054155+rupalee-kumari@users.noreply.github.com>
2025-01-19 09:54:59 +0000

    object json

---
+++ 02_basics/04_object.js
@@ -0,0 +1,8 @@
+const course={
+    coursename:"java Script",
+    price:"999",
+    courseInstructor:"rupalee"
+}
+//console.log(course.courseInstructor)
+const{courseInstructor:instructor}=course  //object destructer{}
+console.log(instructor);
